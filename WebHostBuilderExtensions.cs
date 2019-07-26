003aaf222b22167cf14970a1e94b42228a230ff1
sarawutsam28 <sarawut.klu@bru.ac.th>
2019-07-26 16:48:20 +0700

    full code dotnet core heroku

---
+++ WebHostBuilderExtensions.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+namespace dotnetcoreHeroku
+{
+    public static class WebHostBuilderExtensions
+    {
+        public static IWebHostBuilder UsePort(this IWebHostBuilder builder)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+            if (string.IsNullOrEmpty(port))
+            {
+                return builder;
+            }
+            return builder.UseUrls($"http://+:{port}");
+        }
+    }
+}
